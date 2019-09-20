80b1cec33ccad2c0eb1bbb7c8c2df86eda9e4d8a
Evgeni Golov <evgeni@golov.de>
2019-09-20 09:21:08 +0200

    use obal.ApplicationConfig

--- releasetool/__init__.py
+++ releasetool/__init__.py
@@ -4,24 +4,35 @@ import obal
 from pkg_resources import resource_filename
 
 
-def _get_data_path():
+class ApplicationConfig(obal.ApplicationConfig):
     """
-    Return the data path. Houses playbooks and configs.
+    A class describing the where to find various files
     """
-    return os.environ.get('OBAL_DATA', resource_filename(__name__, 'data'))
 
+    @staticmethod
+    def name():
+        """
+        Return the name as shown to the user in the ArgumentParser
+        """
+        return 'releasetool'
 
-def get_inventory_path():
-    """
-    Return the inventory path
-    """
-    return os.environ.get('OBAL_INVENTORY', os.path.join(os.getcwd(), 'package_manifest.yaml'))
+    @staticmethod
+    def data_path():
+        """
+        Return the data path. Houses playbooks and configs.
+        """
+        return os.environ.get('OBAL_DATA', resource_filename(__name__, 'data'))
+
+    @staticmethod
+    def inventory_path():
+        """
+        Return the inventory path
+        """
+        return os.environ.get('OBAL_INVENTORY', os.path.join(os.getcwd(), 'releasetool.yaml'))
 
 
 def main():
-    obal._get_data_path = _get_data_path
-    obal.get_inventory_path = get_inventory_path
-    obal.main()
+    obal.main(application_config=ApplicationConfig)
 
 
 if __name__ == '__main__':
